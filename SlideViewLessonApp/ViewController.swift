27d05da22c2fd6068c8eb3346b6fa6ddc8102acc
HiroUrata <1712026@i.softbank.jp>
2021-08-07 22:08:11 +0900

    スライドするViewの作成　(まだスライドしない)

--- SlideViewLessonApp/ViewController.swift
+++ SlideViewLessonApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+       createSlideView()
+        
     }
 
-
+    func createSlideView(){
+        
+        let slideView = UIView(frame: CGRect(x: self.view.bounds.minX, y: self.view.bounds.minY, width: self.view.frame.size.width / 2, height: self.view.frame.size.height))
+        
+        slideView.backgroundColor = UIColor.systemGreen
+//        slideView.layer.shadowOffset
+        
+        self.view.addSubview(slideView)
+        
+    }
+    
 }
 
